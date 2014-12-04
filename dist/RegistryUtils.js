774f7767bb37478feada5a6135d2edcf4d07cead
Ingo Richter <ingo.richter@gmail.com>
2014-12-03 18:41:29 -0800

    - add missing file

---
+++ dist/RegistryUtils.js
@@ -0,0 +1,44 @@
+
+/*
+ * BracketsExtensionTweetBot
+ * http://github.com/ingorichter/BracketsExtensionTweetBot
+ *
+ * Copyright (c) 2014 Ingo Richter
+ * Licensed under the MIT license.
+ */
+'use strict';
+var BRACKETS_REGISTRY_JSON, Promise, REGISTRY_BASEURL, downloadExtensionRegistry, request, zlib;
+
+Promise = require('bluebird');
+
+request = require('request');
+
+zlib = require('zlib');
+
+REGISTRY_BASEURL = 'https://s3.amazonaws.com/extend.brackets';
+
+BRACKETS_REGISTRY_JSON = "" + REGISTRY_BASEURL + "/registry.json";
+
+downloadExtensionRegistry = function() {
+  return new Promise(function(resolve, reject) {
+    return request({
+      uri: BRACKETS_REGISTRY_JSON,
+      json: true,
+      encoding: null
+    }, function(err, resp, body) {
+      if (err) {
+        return reject(err);
+      } else {
+        return zlib.gunzip(body, function(err, buffer) {
+          if (err) {
+            return reject(err);
+          } else {
+            return resolve(JSON.parse(buffer.toString()));
+          }
+        });
+      }
+    });
+  });
+};
+
+exports.downloadExtensionRegistry = downloadExtensionRegistry;
